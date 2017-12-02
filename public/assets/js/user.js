87d8fb556bba5242446d4e103453a8a38fbe67dc
Firas nasr <firasnasr@Firass-MacBook-Pro.local>
2017-12-02 11:38:44 -0500

    Firas additional changes

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,3 +1,5 @@
+var geocodeApi = "AIzaSyBUOuAwLVAbvO0rtxxLDyeJlLN4uyESD-I"
+
 $(()=>{
     var message = '';
 
@@ -44,6 +46,7 @@ $(()=>{
         var phone = $('.phone-signup').val().trim();
         var cat = false;
         var dog = false;
+        
 
         // if cat checkbox is checked
         if ($('.cat-signup').is(':checked')) {
@@ -61,8 +64,21 @@ $(()=>{
             $('.signup-notice').html('<div class="alert alert-danger" role="alert">' + message + '</div>');
             return;
         }
+
         // if all fields are filled
         else {
+
+            //this does the geocoding for the address, turning it into a long and lat. 
+            $.ajax({
+            url:  "https://maps.googleapis.com/maps/api/geocode/json?address="+ address +"&key="+ geocodeApi,
+            method: "GET"
+              }).done(function(response) {
+                //latLong variable reaches in for the exact lat and long that comes back for the specific zipcode
+                var latLong =  response.results[0].geometry.location;
+                console.log(latLong);
+                var lat = latLong.lat;
+                var long = latLong.lng;
+
             // create new user object with details
             var newUser = {
                 usertype: 'parent',
@@ -72,7 +88,9 @@ $(()=>{
                 address: address,
                 phone: phone,
                 cat: cat,
-                dog: dog
+                dog: dog,
+                latitude: lat,
+                longitude: long 
             };
 
             // send signup request with new user's details
@@ -105,8 +123,10 @@ $(()=>{
                     $('body').html(content);
                 });
             });
+        });
         }
     });
+       
 
     // sign up as shelter
     $('.btn-signup-shelter').on('click', (e)=>{
@@ -124,8 +144,22 @@ $(()=>{
             $('.signup-notice').html('<div class="alert alert-danger" role="alert">' + message + '</div>');
             return;
         }
+        
+        
         // if all fields are filled
         else {
+        
+        //this does the geocoding for the address, turning it into a long and lat. 
+            $.ajax({
+            url:  "https://maps.googleapis.com/maps/api/geocode/json?address="+ address +"&key="+ geocodeApi,
+            method: "GET"
+                }).done(function(response) {
+                //latLong variable reaches in for the exact lat and long that comes back for the specific zipcode
+                var latLong =  response.results[0].geometry.location;
+                console.log(latLong);
+                var lat = latLong.lat;
+                var long = latLong.lng;
+
             // create new user object with details
             var newUser = {
                 usertype: 'shelter',
@@ -133,7 +167,9 @@ $(()=>{
                 email: email,
                 password: password,
                 address: address,
-                phone: phone
+                phone: phone,
+                latitude: lat,
+                longitude: long 
             };
 
             // send signup request with new user's details
@@ -166,6 +202,7 @@ $(()=>{
                     $('body').html(content);
                 });
             });
+        });
         }
     });
 
